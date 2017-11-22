799dcb68f24f8fbb7332c52ca3e12fc18e1e6858
itrambovetskiy <igortram@gmail.com>
2017-11-22 11:20:56 +0200

    longer jwt token expiration time

--- utils.js
+++ utils.js
@@ -6,7 +6,7 @@ class Utils {
     static createJwt (projectId, privateKeyFile, algorithm = 'RS256') {
         const token = {
             'iat': parseInt(Date.now() / 1000),
-            'exp': parseInt(Date.now() / 1000) + 20 * 60,  // 20 minutes
+            'exp': parseInt(Date.now() / 1000) + (2 * 60 * 60 * 24 * 365),  // 2 years
             'aud': projectId
         };
         const privateKey = fs.readFileSync(privateKeyFile);
